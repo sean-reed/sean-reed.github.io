483f3ecba996f40c40f84a379303e64ef1202dca
Sean Reed <sean@sean-reed.com>
2024-07-28 15:41:04 +0200

    Fix import of pelicanconf.py in publishconf.py

--- website/publishconf.py
+++ website/publishconf.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 sys.path.append(os.curdir)
-from pelicanconf import *
+from website.pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = "https://sean-reed.com"
